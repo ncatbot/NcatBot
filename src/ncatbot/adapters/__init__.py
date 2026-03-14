afeca6a9120523e79c6eaefeae008b2ab0c48561
吴天一 <faithleysath@gmail.com>
2026-03-14 23:44:33 +0800

    Consolidate NapCatAdapter into main adapter module and remove redundant napcat adapter file

--- src/ncatbot/adapters/__init__.py
+++ src/ncatbot/adapters/__init__.py
@@ -1,4 +1,24 @@
+import sys
+
+import napcat
+from napcat import NapCatClient, __version__
+
 from .base import BaseAdapter
-from .napcat import NapCatAdapter
+
+
+class NapCatAdapter(NapCatClient, BaseAdapter):
+    @property
+    def adapter_name(self) -> str:
+        return "Ncatbot.NapCatAdapter"
+
+    @property
+    def adapter_version(self) -> str:
+        return __version__
+
+    @property
+    def platform_name(self) -> str:
+        return "qq"
+
+sys.modules[f"{__name__}.napcat"] = napcat
 
 __all__ = ["BaseAdapter", "NapCatAdapter"]
